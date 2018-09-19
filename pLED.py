4819e6c25b5d19f7f909783d25b8b60f0d400648
isidro704 <isidro.007@outlook.com>
2018-09-19 08:45:01 -0500

    Corrección en pLED.py

--- pLED.py
+++ pLED.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as G
 import time as t
-import cv2 as cv
 
 G.setmode(G.BOARD)
 G.setup(5, G.OUT)
@@ -9,8 +8,4 @@ while True:
     G.output(5, True)
     t.sleep(.1)
     G.output(5, False)
-    t.sleep(.1)
-    
-    k=cv.waitKey(1) & 0xFF
-    if k==27:
-	 break
+    t.sleep(.01)
