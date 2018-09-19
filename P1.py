580bf1fb1745961f3a8aeade48bbd211a5f64cea
isidro704 <isidro.007@outlook.com>
2018-09-19 13:07:47 -0500

    El programa P1.py es nuestra primera prueba con el servomotor, el boton y los dos leds

---
+++ P1.py
@@ -0,0 +1,45 @@
+import RPi.GPIO as g
+import time as t
+
+g.setwarnings(False)
+g.cleanup()
+g.setmode(g.BOARD)
+g.setup(3,g.OUT)#LED final
+g.setup(5,g.OUT)#LED inicio
+g.setup(7,g.OUT)#salida a servo
+g.setup(11,g.IN)#PushButton
+
+sv=g.PWM(7,50)
+sv.start(7.5)
+t.sleep(3)
+
+bt=g.input(11)
+
+p=2.5
+c=0.555556
+
+while True:
+    for i in range(18):
+        if bt==True:
+            t.sleep(0.3)
+            continue
+        if i=0:
+            G.output(3, True)
+            elif i=17:
+                G.output(17, True)
+                else G.output(17, False)
+            else G.output(3, False)
+        p=p+c
+        sv.ChangeDutyCycle(p)
+    for i in range(18):
+        if bt==True:
+            t.sleep(0.3)
+            continue
+        if i=0:
+            G.output(3, True)
+            elif i=17:
+                G.output(17, True)
+                else G.output(17, False)
+            else G.output(3, False)
+        p=p-c
+        sv.ChangeDutyCycle(p)
