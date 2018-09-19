3b76f392a21bb8f2c4ba776845cc38ff6e62f6ca
isidro704 <isidro.007@outlook.com>
2018-09-19 08:59:24 -0500

    Corrección en servo.py

--- servo.py
+++ servo.py
@@ -1,22 +1,22 @@
+import RPi.GPIO as g
 import pigpio as pig
 import time as t
 
 pi=pig.pi() #conecxión con RPi
 
-pi.set_mode(2,pig.OUTPUT) #modo de GPIO
+pi.set_mode(3,pig.OUTPUT) #modo de GPIO
 
-pi.set_servo_pulsewidth(2,1500) #inicio del servo
+pi.set_servo_pulsewidth(3,1500) #inicio del servo
 t.sleep(1)
 
 #recorrido entre -90° y 90°
 for _i in range(5):
-    pi.set_servo_pulsewidth(2,2500)
-    t.sleep(1)
-    pi.set_servo_pulsewidth(2,600)
-    t.sleep(1)
-    pi.set_servo_pulsewidth(2,1500)
-    t.sleep(1)
+    pi.set_servo_pulsewidth(3,2500)
+    t.sleep(2)
+    pi.set_servo_pulsewidth(3,600)
+    t.sleep(2)
+    pi.set_servo_pulsewidth(3,1500)
+    t.sleep(2)
     
-pi.set_servo_pulsewidth(2,0)#finalizacion de pulsos
-
+pi.set_servo_pulsewidth(3,0)#finalizacion de pulsos
 pi.stop()
