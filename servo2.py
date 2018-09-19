5182ffea07093a707741e60fe119ba9a19e0a984
isidro704 <isidro.007@outlook.com>
2018-09-19 09:03:04 -0500

    Corrección en servo2.py

--- servo2.py
+++ servo2.py
@@ -3,15 +3,15 @@ import time as t
 
 sv=pwm.Servo()
 
-sv=set_servo(2,1500)
-t.sleep(1)
+sv=set_servo(3,1500)
+t.sleep(3)
 for _i in range(10):
-    sv.set_servo(2,2500)
-    t.sleep(1)
-    sv.set_servo(2,600)
-    t.sleep(1)
-    sv.set_servo(2,1500)
-    t.sleep(1)
+    sv.set_servo(3,2500)
+    t.sleep(2)
+    sv.set_servo(3,600)
+    t.sleep(2)
+    sv.set_servo(3,1500)
+    t.sleep(2)
     
 sv.set_servo(2,1500)
 t.sleep(1)
