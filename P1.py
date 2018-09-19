df0b5e64ab1e5e93ad44115eb79c26d734668223
isidro704 <isidro.007@outlook.com>
2018-09-19 15:56:05 -0500

    Correción de P1.py

--- P1.py
+++ P1.py
@@ -3,47 +3,48 @@ import time as t
 
 g.setwarnings(False)
 g.cleanup()
+
 g.setmode(g.BOARD)
-g.setup(13,g.OUT)#LED final
-g.setup(15,g.OUT)#LED inicio
-g.setup(7,g.OUT)#salida a servo
-g.setup(11,g.IN)#PushButton
+g.setup(13,g.OUT)   #LED final
+g.setup(15,g.OUT)   #LED inicio
+g.setup(7,g.OUT)    #salida a servo
+g.setup(11,g.IN)    #PushButton
 
 sv=g.PWM(7,50)
 sv.start(7.5)
 t.sleep(3)
 
-bt=g.input(11)
+B=g.input(11)
 
 p=2.5
 c=0.555556
 
 while True:
     for i in range(18):
-        if bt==True:
+        if B==True:
             t.sleep(0.3)
             continue
         if i==0:
             G.output(3, True)
             if i==17:
                 G.output(17, True)
-                else:
-                    G.output(17, False)
-            else:
-                G.output(3, False)
+            else 
+                G.output(17, False)
+        else
+            G.output(3, False)
         p=p+c
         sv.ChangeDutyCycle(p)
     for i in range(18):
-        if bt==True:
+        if B==True:
             t.sleep(0.3)
             continue
         if i==0:
             G.output(3, True)
             if i==17:
                 G.output(17, True)
-                else:
-                    G.output(17, False)
-            else:
-                G.output(3, False)
+            else
+                G.output(17, False)
+        else
+            G.output(3, False)
         p=p-c
         sv.ChangeDutyCycle(p)
