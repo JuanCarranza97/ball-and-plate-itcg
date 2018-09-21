5d6284ac9e7a414e1a20bdd7fa302c1f8590026e
isidro704 <isidro.007@outlook.com>
2018-09-21 10:58:51 -0500

    ...

--- of2.py
+++ of2.py
@@ -1,20 +1,29 @@
-import RPI:GPIO as g
+import RPi.GPIO as g
 import time
-g.setmode(g.BCM)
-g.setup(24, g.in)
-g.setup(18, g.out)
+
+g.setwarnings(False)
+g.setmode(g.BOARD)
+g.setup(3, g.IN)
+g.setup(5, g.IN)
+
 while True:
-    inputValue = g.input(24)
-    if (inputValue == True):
-        g.output(18, g.HIGH)
-        print("Encendido")
-        time.sleep(0.3) #Tiempo necesario para eliminar los rebotes del boton
-        break
-while True
-     inputValueg.input(24)
-     if (inputValue == True):
-         g.output(18, g.LOW)
-         print("Apagado")
-         time.sleep(0.3) #Tiempo necesario para eliminar los rebotes del boton
-         break
+	c=-90
+	p=2.5
+	i=0.555555555556
+	i1 = g.input(3)
+	i2 = g.input(5)
+	if (i1 == True):
+		c=c+10
+		p=p+i
+		print(p)
+		print("Posicion " + str(c) + " up")
+		time.sleep(0.3) #Tiempo necesario para eliminar los rebotes del boton
+		continue
+	if (i2 == True):
+		c=c-10
+		p=p-i
+		print(p)
+		print("Posicion " + str(c) + " down")
+		time.sleep(0.3) #Tiempo necesario para eliminar los rebotes del boton
+		continue
 g.cleanup()
