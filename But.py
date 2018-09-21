9c3daab0155a483a3340b6dbb6fa858956ba8b79
isidro704 <isidro.007@outlook.com>
2018-09-21 10:07:22 -0500

    ...

--- But.py
+++ But.py
@@ -1,16 +1,23 @@
+print('Iniciando programa')
 import RPi.GPIO as g
 import time as t
+t.sleep(.5)
 
-g.setmode(g.BCM)
-g.setup(15, g.IN)
-g.setup(19, g.IN)
+print('Definiendo entradas de tarjeta')
+g.setmode(g.BOARD)
+g.setup(16, g.IN)
+g.setup(18, g.IN)
+t.sleep(.5)
 
+print('Declarando variables')
 c = 0
 p=2.5
 i=0.555556
-B1 = g.input(15)
-B2 = g.input(19)
+B1 = g.input(16)
+#B2 = g.input(18)
+t.sleep(.5)
 
+print('Iniciando ejecucion')
 while True:
 	if B1 == True:
 		c = c + 1
@@ -19,6 +26,8 @@ while True:
 		print(p)
 		print(c)
 		t.sleep(0.3)
+		if c == 18:
+			continue
 	t.sleep(0.01)
 #	elif B2 == True:
 #		c = c - 1
