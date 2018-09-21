726943771d15cd8218d4c3d8ea7d47adcb159551
isidro704 <isidro.007@outlook.com>
2018-09-21 09:16:19 -0500

    ...

--- P1.py
+++ P1.py
@@ -1,20 +1,20 @@
-import RPi.GPIO as g
+#import RPi.GPIO as g
 import time as t
 
-g.setwarnings(False)
-g.cleanup()
+#g.setwarnings(False)
+#g.cleanup()
 
-g.setmode(g.BOARD)
-g.setup(13,g.OUT)   #LED final
-g.setup(15,g.OUT)   #LED inicio
-g.setup(7,g.OUT)    #salida a servo
-g.setup(11,g.IN)    #PushButton
+#g.setmode(g.BOARD)
+#g.setup(13,g.OUT)   #LED final
+#g.setup(15,g.OUT)   #LED inicio
+#g.setup(7,g.OUT)    #salida a servo
+#g.setup(11,g.IN)    #PushButton
 
-sv=g.PWM(7,50)
-sv.start(7.5)
-t.sleep(3)
+#sv=g.PWM(7,50)
+#sv.start(7.5)
+#t.sleep(3)
 
-B=g.input(11)
+B=input(1)
 
 p=2.5
 c=0.555556
