f1b808852aa744ab8e0d6f292613b2cf5df2b8f8
isidro704 <isidro.007@outlook.com>
2018-09-21 13:00:00 -0500

    ...

--- of2.py
+++ of2.py
@@ -5,11 +5,12 @@ g.setwarnings(False)
 g.setmode(g.BOARD)
 g.setup(3, g.IN)
 g.setup(5, g.IN)
+c=-90
+p=2.5
+i=0.555555555556
 
 while True:
-	c=-90
-	p=2.5
-	i=0.555555555556
+
 	i1 = g.input(3)
 	i2 = g.input(5)
 	if (i1 == True):
@@ -17,13 +18,19 @@ while True:
 		p=p+i
 		print(p)
 		print("Posicion " + str(c) + " up")
-		time.sleep(0.3) #Tiempo necesario para eliminar los rebotes del boton
-		continue
+		time.sleep(0.3) #Tiempo necesario para eliminar los rebotes del boton		
+	if (c>=100) and (p>=12.5):
+	 c=90
+	 p=12.5
+	 continue
 	if (i2 == True):
 		c=c-10
 		p=p-i
 		print(p)
 		print("Posicion " + str(c) + " down")
 		time.sleep(0.3) #Tiempo necesario para eliminar los rebotes del boton
+	if (c==-100):
+		c=-90
+		p=2.5
 		continue
 g.cleanup()
