4808d4bd7aafd7918ae9dc5786e89b6295f060eb
isidro704 <isidro.007@outlook.com>
2018-09-24 12:43:10 -0500

    ...

--- sbl.py
+++ sbl.py
@@ -16,35 +16,35 @@ p=2.5
 i=0.555555555556
 print('Posicion inicial')
 
-while True:
-	i1 = g.input(3)
-	i2 = g.input(5)
-	sv.ChangeDutyCycle(p)
-	print('Posicion ' + str(c) + ' up')
-	if (i1 == True):
-		c = c + 10
-		p = p + i
-		t.sleep(0.3)
-		sv.ChangeDutyCycle(p)
-		print('Posicion ' + str(c) + ' up')
-	elif (c >= 90):
-		c = 90
-		p = 12.5
-		g.output(13, True)
-		continue
-	else:
-		g.output(13, False)		
-	if (i2 == True):
-		c = c - 10
-		p = p - i
-		t.sleep(0.3)
-		sv.ChangeDutyCycle(p)
-		print('Posicion ' + str(c) + ' down')
-	if (c <= -90):
-		c = -90
-		p = 2.5
-		g.output(11, True)
-		continue
-	else:
-		g.output(11, False)
+def s():
+	while True:
+		i1 = g.input(3)
+		i2 = g.input(5)
+		if (i1 == True):
+			c = c + 10
+			p = p + i
+			t.sleep(0.3)
+			sv.ChangeDutyCycle(p)
+			print('Posicion ' + str(c) + ' up')
+		elif (c >= 90):
+			c = 90
+			p = 12.5
+			g.output(13, True)
+			continue
+		else:
+			g.output(13, False)		
+		if (i2 == True):
+			c = c - 10
+			p = p - i
+			t.sleep(0.3)
+			sv.ChangeDutyCycle(p)
+			print('Posicion ' + str(c) + ' down')
+		if (c <= -90):
+			c = -90
+			p = 2.5
+			g.output(11, True)
+			continue
+		else:
+			g.output(11, False)
+s()
 g.cleanup()
