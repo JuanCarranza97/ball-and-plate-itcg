db4364e096565c28aa57534d646c9ad8fe7fc302
isidro704 <isidro.007@outlook.com>
2018-09-24 11:31:33 -0500

    ...

--- of2.py
+++ of2.py
@@ -1,36 +1,48 @@
 import RPi.GPIO as g
-import time
+import time as t
 
 g.setwarnings(False)
 g.setmode(g.BOARD)
 g.setup(3, g.IN)
 g.setup(5, g.IN)
+g.setup(7,g.OUT)
+g.setup(11,g.OUT)
+g.setup(13,g.OUT)
+
+sv=g.PWM(7,50)
+
 c=-90
 p=2.5
 i=0.555555555556
+print('Posicion inicial')
 
 while True:
-
 	i1 = g.input(3)
 	i2 = g.input(5)
 	if (i1 == True):
-		c=c+10
-		p=p+i
-		print(p)
-		print("Posicion " + str(c) + " up")
-		time.sleep(0.3) #Tiempo necesario para eliminar los rebotes del boton		
-	if (c>=100) and (p>=12.5):
-	 c=90
-	 p=12.5
-	 continue
-	if (i2 == True):
-		c=c-10
-		p=p-i
-		print(p)
-		print("Posicion " + str(c) + " down")
-		time.sleep(0.3) #Tiempo necesario para eliminar los rebotes del boton
-	if (c==-100):
-		c=-90
-		p=2.5
+		print('Posicion ' + str(c) + ' up')
+		sv.ChangeDutyCycle(p)
+		c = c + 10
+		p = p + i
+		t.sleep(0.3)
+	if (c >= 90):
+		c = 90
+		p = 12.5
+		g.output(13, True)
 		continue
+	else:
+		g.output(13, False)		
+	if (i2 == True):
+		print('Posicion ' + str(c) + ' down')
+		sv.ChangeDutyCycle(p)
+		c = c - 10
+		p = p - i
+		t.sleep(0.3)
+	if (c <= -90):
+		c = -90
+		p = 2.5
+		g.output(11, True)
+		continue
+	else:
+		g.output(11, False)
 g.cleanup()
