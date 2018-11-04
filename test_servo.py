8551b428f097b76d3a89bcc6607586b27756ffc3
isidro704 <isidro.007@outlook.com>
2018-11-03 18:44:05 -0600

    Se agrego posicion de home

--- test_servo.py
+++ test_servo.py
@@ -11,14 +11,26 @@ pca = PCA9685(i2c)
 
 pca.frequency = 50
 
+min_servo_signal = [20,25,25,23,35,20]
+max_servo_signal = [118,115,115,108,125,110]
+
+min_degree = [0,90,0,90,0,90]
+max_degree = [90,180,90,180,90]
+
+home_degree = [min_servo_signal[0],max_servo_signal[1],min_servo_signal[2],max_servo_signal[3],min_servo_signal[4],max_servo_signal[5]]
+
 a=800		#Pulso minimo
 b=2700		#Pulso maximo
 
 servos = [servo.Servo(pca.channels[0], min_pulse=a, max_pulse=b)]
+servos[0].angle = home_degree[0]
+loop=1
 for i in [1,4,5,6,7]:
 	#print("Configurando pin {}".format(i))
 	servos.append(servo.Servo(pca.channels[i], min_pulse=a, max_pulse=b)) #Servomotor MG995
-
+	servos[loop].angle = home_degree[loop]
+	loop+=1
+	
 while True:
 	try:
 		entrada = input("Set the selected servo to specific position:\n")
