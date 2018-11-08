1e22fee709fc2998f1df385a9297c9311b92876c
isidro704 <isidro.007@outlook.com>
2018-11-08 13:03:15 -0600

    modificación

--- 6servo.py
+++ 6servo.py
@@ -14,50 +14,56 @@ pca.frequency = 50
 a=800		#Pulso minimo
 b=2700		#Pulso maximo
 
-ser1 = servo.Servo(pca.channels[ 0], min_pulse=a, max_pulse=b) #Servomotor MG995
-ser2 = servo.Servo(pca.channels[ 1], min_pulse=a, max_pulse=b)
-ser3 = servo.Servo(pca.channels[ 4], min_pulse=a, max_pulse=b)
-ser4 = servo.Servo(pca.channels[ 5], min_pulse=a, max_pulse=b)
-ser5 = servo.Servo(pca.channels[ 6], min_pulse=a, max_pulse=b)
-ser6 = servo.Servo(pca.channels[ 7], min_pulse=a, max_pulse=b)
+ser0 = servo.Servo(pca.channels[ 0], min_pulse=a, max_pulse=b) #Servomotor MG995
+ser1 = servo.Servo(pca.channels[ 1], min_pulse=a, max_pulse=b)
+ser2 = servo.Servo(pca.channels[ 4], min_pulse=a, max_pulse=b)
+ser3 = servo.Servo(pca.channels[ 5], min_pulse=a, max_pulse=b)
+ser4 = servo.Servo(pca.channels[ 6], min_pulse=a, max_pulse=b)
+ser5 = servo.Servo(pca.channels[ 7], min_pulse=a, max_pulse=b)
 
-ser1.angle = 0
+m=180
+p1=135
+p2=170
+p3=90
+ser0.angle = p3
 t.sleep(0.5)
-ser2.angle = 0
+ser1.angle = m - p3
 t.sleep(0.5)
-ser3.angle = 0
+ser2.angle = p1
 t.sleep(0.5)
-ser4.angle = 0
+ser3.angle = m - p3
 t.sleep(0.5)
-ser5.angle = 0
+ser4.angle = p2
 t.sleep(0.5)
-ser6.angle = 0
+ser5.angle = m - p1
 t.sleep(0.5)
 
+'''
 while True:
-	for i in range(0,180,5):
-		ser1.angle = i
+	for i in range(90,180,2):
+		ser0.angle = i
+		t.sleep(0.05)
+		ser1.angle = m - i
 		t.sleep(0.05)
 		ser2.angle = i
 		t.sleep(0.05)
-		ser3.angle = i
+		ser3.angle = m - i
 		t.sleep(0.05)
 		ser4.angle = i
 		t.sleep(0.05)
-		ser5.angle = i
+		ser5.angle = m - i 
 		t.sleep(0.05)
-		ser6.angle = i
+	for i in range(180,90,2):
+		ser0.angle = i
 		t.sleep(0.05)
-	for i in range(0,180,5):
-		ser1.angle = 180 - i
+		ser1.angle = m - i
 		t.sleep(0.05)
-		ser2.angle = 180 - i
+		ser2.angle = i
 		t.sleep(0.05)
-		ser3.angle = 180 - i
+		ser3.angle = m - i
 		t.sleep(0.05)
-		ser4.angle = 180 - i
+		ser4.angle = i
 		t.sleep(0.05)
-		ser5.angle = 180 - i
+		ser5.angle = m - i
 		t.sleep(0.05)
-		ser6.angle = 180 - i
-		t.sleep(0.05)
+'''
